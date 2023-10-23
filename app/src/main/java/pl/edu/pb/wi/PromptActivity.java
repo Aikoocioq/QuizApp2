ae37d6fb9ed843f515380c8354b69df338620eba
Aikoocioq <ola.didluch@o2.pl>
2023-10-23 10:38:41 +0200

    Quiz2-poprawki

--- app/src/main/java/pl/edu/pb/wi/PromptActivity.java
+++ app/src/main/java/pl/edu/pb/wi/PromptActivity.java
@@ -13,6 +13,7 @@ import android.widget.Toast;
 public class PromptActivity extends AppCompatActivity {
     private boolean correctAnswer;
     private Button confirmButton;
+    private Button backButton;
     private TextView answerTextView;
     public static final String KEY_EXTRA_ANSWER_SHOWN = "answerShown";
     @Override
@@ -21,6 +22,7 @@ public class PromptActivity extends AppCompatActivity {
         setContentView(R.layout.activity_prompt);
         answerTextView = findViewById(R.id.answer_text_view);
         confirmButton = findViewById(R.id.confirm_button);
+        backButton = findViewById(R.id.back_button);
         correctAnswer = getIntent().getBooleanExtra(MainActivity.KEY_EXTRA_ANSWER, true);
         confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,6 +30,12 @@ public class PromptActivity extends AppCompatActivity {
                 int answer = correctAnswer ? R.string.button_true : R.string.button_false;
                 answerTextView.setText(answer);
                 setAnswerShownResult(true);
+            }
+        });
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
     }
